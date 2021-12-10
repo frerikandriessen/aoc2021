1f5f01da95ec7e35101319b6ea0e5e7f984e42b0
Frerik Andriessen <frederik.dionysus.andriessen@alliander.com>
2021-12-10 16:50:37 +0100

    Add option to main.py to automatically retrieve the puzzle input

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import importlib
 from pathlib import Path
 import argparse
+import textwrap
+
+import requests
 
 parser = argparse.ArgumentParser(
     "Advent of Code 2021 utility script\n\n👀 Gives answers to the questions you have 👀"
@@ -9,7 +12,41 @@ parser.add_argument("DAY", type=int, help="The day that you want the solutions f
 parser.add_argument(
     "--test", action="store_true", help="Runs the algorithm on test data"
 )
+parser.add_argument(
+    "--create", action="store_true", help="Creates a folder for this day and downloads your input"
+)
 args = parser.parse_args()
+
+
+def create_new_day_folder(day: int):
+    directory = Path(__file__).parent / f"day{day}"
+    directory.mkdir(exist_ok=False)
+    (directory / "__init__.py").touch()
+    (directory / "test_input.txt").touch()
+
+
+    standard_solutions_content = textwrap.dedent(
+        """\
+        from typing import List
+
+
+        def q1(data: List[str]) -> int:
+            return 0
+
+        def q2(data: List[str]) -> int:
+            return 0
+        """
+    )
+    with (directory / "solution.py").open("w", newline="\n") as f:
+        f.write(standard_solutions_content)
+
+    aoc_cookie = (Path().parent / "aoc_cookie").read_text()
+
+    r = requests.get(f"https://adventofcode.com/2021/day/{day}/input", headers={"cookie": aoc_cookie})
+    r.raise_for_status()
+    with (directory / "input.txt").open("w", newline="\n") as f:
+        f.write(r.text)
+
 
 
 def preprocess_data(data):
@@ -18,6 +55,10 @@ def preprocess_data(data):
 
 if __name__ == "__main__":
     day = args.DAY
+
+    if args.create:
+        create_new_day_folder(day)
+        exit(0)
 
     if args.test:
         input_data_path = Path(f"./day{day}/test_input.txt")
